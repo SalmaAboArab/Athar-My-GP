221da02a687a77588053d57cae2792c0aff78b1e
Salma Hamdy <eng.salma.hamdy@gmail.com>
2023-06-12 05:29:25 +0300

    deploy10

--- src/modules/auth/controller/auth.js
+++ src/modules/auth/controller/auth.js
@@ -1182,74 +1182,451 @@ export const sendCode = asyncHandler(async (req, res, next) => {
     if (!user) {
         return next(new Error("Not register account", { cause: 404 }))
     }
-    const html = `<!DOCTYPE html>
-    <html>
-    <head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></head>
-    <style type="text/css">
-    body{background-color: #88BDBF;margin: 0px;}
-    </style>
-    <body style="margin:0px;"> 
-    <table border="0" width="50%" style="margin:auto;padding:30px;background-color: #F3F3F3;border:1px solid #630E2B;">
-    <tr>
-    <td>
-    <table border="0" width="100%">
-    <tr>
-    <td>
-    <h1>
-        <img width="100px" src="${process.env.logo}"/>
-    </h1>
-    </td>
-    </tr>
-    </table>
-    </td>
-    </tr>
-    <tr>
-    <td>
-    <table border="0" cellpadding="0" cellspacing="0" style="text-align:center;width:100%;background-color: #fff;">
-    <tr>
-    <td style="background-color:#630E2B;height:100px;font-size:50px;color:#fff;">
-    <img width="50px" height="50px" src="https://res.cloudinary.com/ddajommsw/image/upload/v1670703716/Screenshot_1100_yne3vo.png">
-    </td>
-    </tr>
-    <tr>
-    <td>
-    <h1 style="padding-top:25px; color:#630E2B">Forget Password</h1>
-    </td>
-    </tr>
-    <tr>
-    <td>
-    <p style="padding:0px 100px;">
-    </p>
-    </td>
-    </tr>
-    <tr>
-    <td>
-    <p style="margin:10px 0px 30px 0px;border-radius:4px;padding:10px 20px;border: 0;color:#fff;background-color:#630E2B; ">${forgetCode}</p>
-    </td>
-    </tr>
-  
-    </table>
-    </td>
-    </tr>
-    <tr>
-    <td>
-    <table border="0" width="100%" style="border-radius: 5px;text-align: center;">
-    <tr>
-    <td>
-    <h3 style="margin-top:10px; color:#000">Stay in touch</h3>
-    </td>
-    </tr>
-    <tr>
-    <td>
-
-    </td>
-    </tr>
-    </table>
-    </td>
-    </tr>
-    </table>
-    </body>
+    const html = `<html>
+        <head>
+        <title></title>
+        <!--[if !mso]><!-- -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!--<![endif]-->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <style type="text/css">
+          #outlook a {
+            padding: 0;
+          }
+          .ReadMsgBody {
+            width: 100%;
+          }
+          .ExternalClass {
+            width: 100%;
+          }
+          .ExternalClass * {
+            line-height: 100%;
+          }
+          body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+          }
+          table,
+          td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+          }
+          img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+          }
+          p {
+            display: block;
+            margin: 13px 0;
+          }
+        </style>
+        <!--[if !mso]><!-->
+        <style type="text/css">
+          @media only screen and (max-width: 480px) {
+            @-ms-viewport {
+              width: 320px;
+            }
+            @viewport {
+              width: 320px;
+            }
+          }
+        </style>
+        <link
+          href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <style type="text/css">
+          @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+        </style>
+        <!--<![endif]-->
+        <style type="text/css">
+          @media only screen and (min-width: 480px) {
+            .mj-column-per-100,
+            * [aria-labelledby="mj-column-per-100"] {
+              width: 100% !important;
+            }
+          }
+        </style>
+      </head>
+      <body style="background: #f9f9f9">
+        <div style="background-color: #f9f9f9">
+          <style type="text/css">
+            html,
+            body,
+            * {
+              -webkit-text-size-adjust: none;
+              text-size-adjust: none;
+            }
+            a {
+              color: #1eb0f4;
+              text-decoration: none;
+            }
+            a:hover {
+              text-decoration: underline;
+            }
+          </style>
+          <div style="margin: 0px auto; max-width: 640px; background: transparent">
+            <table
+              role="presentation"
+              cellpadding="0"
+              cellspacing="0"
+              style="font-size: 0px; width: 100%; background: transparent"
+              align="center"
+              border="0"
+            >
+              <tbody>
+                <tr>
+                  <td
+                    style="
+                      text-align: center;
+                      vertical-align: top;
+                      direction: ltr;
+                      font-size: 0px;
+                      padding: 40px 0px;
+                    "
+                  >
+                    <div
+                      aria-labelledby="mj-column-per-100"
+                      class="mj-column-per-100 outlook-group-fix"
+                      style="
+                        vertical-align: top;
+                        display: inline-block;
+                        direction: ltr;
+                        font-size: 13px;
+                        text-align: left;
+                        width: 100%;
+                      "
+                    >
+                      <table
+                        role="presentation"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        border="0"
+                      >
+                        <tbody>
+                          <tr>
+                            <td
+                              style="
+                                word-break: break-word;
+                                font-size: 0px;
+                                padding: 0px;
+                              "
+                              align="center"
+                            >
+                              <table
+                                role="presentation"
+                                cellpadding="0"
+                                cellspacing="0"
+                                style="border-collapse: collapse; border-spacing: 0px"
+                                align="center"
+                                border="0"
+                              >
+                                <tbody>
+                                  <tr>
+                                    <td style="width: 138px">
+                                     
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div
+            style="
+              max-width: 640px;
+              margin: 0 auto;
+              box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);
+              border-radius: 4px;
+              overflow: hidden;
+            "
+          >
+            <div
+              style="
+                margin: 0px auto;
+                max-width: 640px;
+                background: #7289da
+                  url(https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png)
+                  top center / cover no-repeat;
+              "
+            >
+              <table
+                role="presentation"
+                cellpadding="0"
+                cellspacing="0"
+                style="
+                  font-size: 0px;
+                  width: 100%;
+                  background: #7289da
+                    url(https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png)
+                    top center / cover no-repeat;
+                "
+                align="center"
+                border="0"
+                background="https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png"
+              >
+                <tbody>
+                  <tr>
+                    <td
+                      style="
+                        text-align: center;
+                        vertical-align: top;
+                        direction: ltr;
+                        font-size: 0px;
+                        padding: 57px;
+                      "
+                    >
+                      <div
+                        style="
+                          cursor: auto;
+                          color: white;
+                          font-family: Whitney, Helvetica Neue, Helvetica, Arial,
+                            Lucida Grande, sans-serif;
+                          font-size: 36px;
+                          font-weight: 600;
+                          line-height: 36px;
+                          text-align: center;
+                        "
+                      >
+                      Is that you!
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div style="margin: 0px auto; max-width: 640px; background: #ffffff">
+              <table
+                role="presentation"
+                cellpadding="0"
+                cellspacing="0"
+                style="font-size: 0px; width: 100%; background: #ffffff"
+                align="center"
+                border="0"
+              >
+                <tbody>
+                  <tr>
+                    <td
+                      style="
+                        text-align: center;
+                        vertical-align: top;
+                        direction: ltr;
+                        font-size: 0px;
+                        padding: 40px 70px;
+                      "
+                    >
+                      <div
+                        aria-labelledby="mj-column-per-100"
+                        class="mj-column-per-100 outlook-group-fix"
+                        style="
+                          vertical-align: top;
+                          display: inline-block;
+                          direction: ltr;
+                          font-size: 13px;
+                          text-align: left;
+                          width: 100%;
+                        "
+                      >
+                        <table
+                          role="presentation"
+                          cellpadding="0"
+                          cellspacing="0"
+                          width="100%"
+                          border="0"
+                        >
+                          <tbody>
+                            <tr>
+                              <td
+                                style="
+                                  word-break: break-word;
+                                  font-size: 0px;
+                                  padding: 0px 0px 20px;
+                                "
+                                align="left"
+                              >
+                                <div
+                                  style="
+                                    cursor: auto;
+                                    color: #737f8d;
+                                    font-family: Whitney, Helvetica Neue, Helvetica,
+                                      Arial, Lucida Grande, sans-serif;
+                                    font-size: 16px;
+                                    line-height: 24px;
+                                    text-align: left;
+                                  "
+                                >
+                                  <p>
+                                    <img
+                                      src="https://res.cloudinary.com/dfzbcjlal/image/upload/v1686483249/vbptqaqwrfnoiyuelgbk.png"
+                                      alt="Party Wumpus"
+                                      title="None"
+                                      width="500"
+                                      style="height: auto"
+                                    />
+                                  </p>
+      
+                                  <h2
+                                    style="
+                                      font-family: Whitney, Helvetica Neue, Helvetica,
+                                        Arial, Lucida Grande, sans-serif;
+                                      font-weight: 500;
+                                      font-size: 20px;
+                                      color: #4f545c;
+                                      letter-spacing: 0.27px;
+                                    "
+                                  >
+                                    Hey ${user.name},
+                                  </h2>
+                                  <p>This is your verification code : </p>
+                                </div>
+                                <br> <br>
+                            <button style="
+                                      border: none;
+                                      border-radius: 3px;
+                                      color: white;
+                                      cursor: auto;
+                                      padding: 15px 19px;
+                                      position: relative;
+                                      left: 40%;
+                                      /* align:center; */
+                                      /* valign:middle; */
+                                      background-color:#7289DA;
+                                    "
+                                    >${forgetCode}
+                                  </button>
+                              </td>
+                            </tr>
+                            
+                          </tbody>
+                        </table>
+                      </div>
+                      <!--[if mso | IE]>
+              </td></tr></table>
+              <![endif]-->
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div style="margin: 0px auto; max-width: 640px; background: transparent">
+            <table
+              role="presentation"
+              cellpadding="0"
+              cellspacing="0"
+              style="font-size: 0px; width: 100%; background: transparent"
+              align="center"
+              border="0"
+            >
+              <tbody>
+                <tr>
+                  <td
+                    style="
+                      text-align: center;
+                      vertical-align: top;
+                      direction: ltr;
+                      font-size: 0px;
+                      padding: 0px;
+                    "
+                  >
+                    <div
+                      aria-labelledby="mj-column-per-100"
+                      class="mj-column-per-100 outlook-group-fix"
+                      style="
+                        vertical-align: top;
+                        display: inline-block;
+                        direction: ltr;
+                        font-size: 13px;
+                        text-align: left;
+                        width: 100%;
+                      "
+                    >
+                      <table
+                        role="presentation"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        border="0"
+                      >
+                        <tbody>
+                          <tr>
+                            <td style="word-break: break-word; font-size: 0px">
+                              <div style="font-size: 1px; line-height: 12px">
+                                &nbsp;
+                              </div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div
+            style="
+              margin: 0 auto;
+              max-width: 640px;
+              background: #ffffff;
+              box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);
+              border-radius: 4px;
+              overflow: hidden;
+            "
+          >
+            <table
+              cellpadding="0"
+              cellspacing="0"
+              style="font-size: 0px; width: 100%; background: #ffffff"
+              align="center"
+              border="0"
+            >
+              <tbody>
+                <tr>
+                  <td
+                    style="
+                      text-align: center;
+                      vertical-align: top;
+                      font-size: 0px;
+                      padding: 0px;
+                    "
+                  >
+                    <div
+                      aria-labelledby="mj-column-per-100"
+                      class="mj-column-per-100 outlook-group-fix"
+                      style="
+                        vertical-align: top;
+                        display: inline-block;
+                        direction: ltr;
+                        font-size: 13px;
+                        text-align: left;
+                        width: 100%;
+                      "
+                    >
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </body>
     </html>`
     if (!await sendEmail({ to: email, subject: 'Forget Password', html })) {
         return next(new Error("Email rejected", { cause: 400 }))
